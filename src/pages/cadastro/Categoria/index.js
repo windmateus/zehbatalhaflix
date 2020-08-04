5050b8e5ae400cb7931633e6d892f89e9a7dd618
Windson Mateus <windmateus@gmail.com>
2020-08-03 22:13:58 -0300

    Aula 3!

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,20 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+      nome: '',
+      descricao: '',
+      cor: ''
+    }
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+      setValues({
+        ...values, 
+        [chave]: valor,   //nome: 'valor'   -  com os colchetes a chave é criada de forma dinâmica
+      })
+    }
+
+    function handleChange(infoEvento) {
+      const { getAttribute, value } = infoEvento.target;  // refatoração bacana mas não funcionou...
+      setValue(
+        infoEvento.target.getAttribute('name'), 
+        infoEvento.target.value
+      );
+    }    
+
     return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
+        <h1>Cadastro de Categoria: {values.nome}</h1>
 
-        <form>
+        <form onSubmit={function handleSubmit(infoEvento) {
+            infoEvento.preventDefault();
+            //console.log('tentou enviar o form');
+            setCategorias([
+              ...categorias,
+              values
+            ]);
 
-          <label>
-            Nome da Categoria:
-            <input 
-              type="text" 
-            />
-          </label>
+            setValues(valoresIniciais);
+        }}>
+
+          <FormField 
+            label="Nome da Categoria"
+            type="text"
+            name="nome"          
+            value={values.nome}
+            onChange={handleChange}
+          />
+
+          <div>            
+            <label>
+              Descrição:
+              <textarea 
+                type="text" 
+                value={values.descricao}
+                name="descricao"
+                onChange={handleChange}
+              />
+            </label>
+            </div>            
+
+          <FormField 
+            label="Cor"          
+            type="color"
+            name="cor"
+            value={values.cor}
+            onChange={handleChange}
+          />
 
           <button>
             Cadastrar
@@ -22,6 +76,18 @@ function CadastroCategoria() {
 
         </form>
 
+        <ul>
+          {categorias.map((categoria, indice) => {
+            //console.log(categoria);
+            //O indice evita que dê o problema de erro ao cadastrar itens duplicados
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            )
+          }
+          )}
+        </ul>
 
         <Link to="/">
             Ir para Home
