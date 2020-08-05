fb10bc50ed19cc8749d43041288a800a1e7e7e36
Windson Mateus <windmateus@gmail.com>
2020-08-05 13:56:15 -0300

    Aula 4 final

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,8 +30,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    // console.log('hello brazil');
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://zehbatalhaflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
